f70332dc2b523886a31ca73d2991f527b55bd026
kathe <katheeja.yasmin@valuelabs.com>
2024-11-05 08:32:18 +0530

    First Push

---
+++ demoFrameWork/src/test/java/CreateOrganizationTest/ToCreateOrganizationTest.java
@@ -0,0 +1,8 @@
+package CreateOrganizationTest;
+
+public class ToCreateOrganizationTest {
+	public static void main(String[] args) {
+		System.out.println("Organization succesfully created");
+	}
+
+}
